8b8a8f65d0ae2a57467a6512a342f302770efe51
ruuffian <ruuffian@gmail.com>
2022-06-08 21:25:41 -0400

    Project roughly templated

---
+++ README.md
@@ -0,0 +1,4 @@
+# panther-bot
+
+This is a small project to teach me how to write good javascript, as well as 
+interact with databases.
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-let Discord = require("discord.js")
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client,Collection, Intents} = require('discord.js');
+const {token} = require('./config.json');
 
-console.log("Hello World")
+// Create client instance
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+
+// dynamically load commands from */commands directory
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+// Run client
+client.once('ready', () => {
+    console.log('Client successfully running!');
+});
+
+// Naive command handling
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Login using token
+client.login(token);
