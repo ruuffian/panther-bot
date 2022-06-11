7adf261b6811e1a722c63e549ba73bf125a71289
ruuffian <ruuffian@gmail.com>
2022-06-11 15:21:17 -0400

    Register command implemented. Known bugs:\n Button press client side says interaction failed when it succeeds\n Any database rejection (duplicate key, key doesnt exist etc) crashes program
    Signed-off-by: ruuffian <ruuffian@gmail.com>

--- commands/register.js
+++ commands/register.js
@@ -1,38 +1,115 @@
+require('dotenv').config();
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { v4: uuidv4 } = require('uuid');
-const { Pool } = require('pg');
-
+const { MessageActionRow, MessageButton } = require('discord.js');
+const { v4 } = require('uuid');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('register')
-		.setDescription('Register your account with Panther Bot!'),
+		.setDescription('Register a team or player with Panther Bot!')
+		.addSubcommand(command =>
+			command
+				.setName('team')
+				.setDescription('Register a team!')
+				.addStringOption(option =>
+					option
+						.setName('team_name')
+						.setDescription('The name of the team to be registered!')
+						.setRequired(true)))
+		.addSubcommand(command =>
+			command
+				.setName('player')
+				.setDescription('Register a player to a team!')
+				.addStringOption(option =>
+					option
+						.setName('player_name')
+						.setDescription('The name of the player to be registered!')
+						.setRequired(true))
+				.addStringOption(option =>
+					option
+						.setName('team_name')
+						.setDescription('The team to register the player to!')
+						.setRequired(true)
+						.addChoices(
+							{ name: 'Econ', value: 'econ' },
+							{ name: 'Exec', value: 'exec' },
+						))),
 	async execute(interaction) {
-		// Insert the user into the lookup table with a generated ID
-		const uuid = uuidv4();
-		const pool = new Pool();
+		const subcommand = interaction.options.getSubcommand();
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('accept')
+					.setLabel('Yes')
+					.setStyle('SUCCESS'),
+				new MessageButton()
+					.setCustomId('decline')
+					.setLabel('No')
+					.setStyle('DANGER'),
+			);
+		switch (subcommand) {
+		case 'team': {
+			await interaction.reply({
+				content: `Registering ${interaction.options.getString('team_name')}?`,
+				components: [row],
+				ephemeral: true,
+			});
 
-		let query = {
-			text: 'INSERT INTO user_lookup VALUES ($1, $2}) RETURNING *',
-			values:[interaction.user.username, uuid],
-		};
-		pool.query(query, (err, res) => {
-			if (err) {
-				throw err;
-			}
-			console.log(`User lookup created: ${res.rows[0]}`);
-		});
-
-		query = {
-			text: 'INSERT INTO users VALUES ($1, $2, $3, $4}) RETURNING *',
-			values: [uuid, interaction.member.permissions.has(['Panther Corps Ghosts', 'Panther Corps Highlanders', 'Panther Corps Sentinels', 'Panther Corps Recruits', 'Honored PC Veterans']), 0],
-		};
-		pool.query(query, (err, res) => {
-			if (err) {
-				throw err;
-			}
-			console.log(`User created: ${res.rows[0]}`);
-		});
-
-		await interaction.reply(`Player ${interaction.user.username} registered!`);
+			interaction.client.once('interactionCreate', async button => {
+				if (!button.isButton()) return;
+				const confirm = button.customId;
+				if (confirm === 'accept') {
+					const db = require('../db');
+					const text = 'INSERT INTO teams VALUES ($1, $2) RETURNING *';
+					const teamid = v4();
+					const teamname = interaction.options.getString('team_name');
+					const values = [teamid, teamname];
+					db.query(text, values, async (err, res) => {
+						if (err) throw err;
+						await interaction.followUp({
+							content:`Team ${res.rows[0].teamname} registered!`,
+							ephemeral: true,
+						});
+					});
+				}
+				else if (confirm === 'decline') {
+					await interaction.followUp('Registration canceled.');
+				}
+			});
+			break;
+		}
+		case 'player': {
+			// confirm input
+			await interaction.reply({
+				content: `Registering ${interaction.options.getString('player_name')} \
+				to team ${interaction.options.getString('team_name')}?`,
+				components: [row],
+				ephemeral: true,
+			});
+			// locally handle button press
+			interaction.client.once('interactionCreate', async button => {
+				if (!button.isButton()) return;
+				const confirm = button.customId;
+				if (confirm === 'accept') {
+					// db query
+					const db = require('../db');
+					const text = 'INSERT INTO users VALUES ($1, $2) RETURNING *';
+					const userid = interaction.options.getString('player_name');
+					const teamid = interaction.options.getString('team_name');
+					const values = [userid, teamid];
+					db.query(text, values, async (err, res) => {
+						if (err) throw err;
+						await interaction.followUp({
+							content:`Player ${res.rows[0].userid} registered for team ${res.rows[0].teamid}!`,
+							ephemeral: true,
+						});
+					});
+				}
+				else if (confirm === 'decline') {
+					await interaction.followUp('Registration canceled.');
+				}
+			});
+			break;
+		}
+		}
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,7 +7,6 @@ module.exports = {
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) return;
-
 		try {
 			await command.execute(interaction);
 		}
