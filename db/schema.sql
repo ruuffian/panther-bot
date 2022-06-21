2ee9facc74ee96371427f531c9a9ea12fc680cb0
ruuffian <ruuffian@gmail.com>
2022-06-21 13:53:32 -0400

    db schema updated to support foreign keys
    Signed-off-by: ruuffian <ruuffian@gmail.com>

--- db/schema.sql
+++ db/schema.sql
@@ -1,42 +1,52 @@
 -- CREATE TYPE POSITION AS ENUM ('TOP', 'JUNG', 'MID', 'ADC', 'SUP');
 
-CREATE TABLE "users" (
-  "userid" CHAR(37) PRIMARY KEY NOT NULL,
-  "teamid" CHAR(37) NOT NULL,
-  "starter" BOOLEAN,
-  "position" varchar(5)
-);
-
 CREATE TABLE "teams" (
-  "teamid" CHAR(37) PRIMARY KEY NOT NULL,
+  "teamid" CHAR(37),
   "captain" VARCHAR(255) NOT NULL,
   "wins" SMALLINT DEFAULT 0,
-  "losses" SMALLINT DEFAULT 0
+  "losses" SMALLINT DEFAULT 0,
+  PRIMARY KEY(teamid)
+);
+
+CREATE TABLE "users" (
+  "userid" CHAR(37),
+  "teamid" CHAR(37),
+  "starter" BOOLEAN NOT NULL,
+  "position" varchar(5) NOT NULL,
+  PRIMARY KEY(userid),
+  CONSTRAINT fk_teamid
+    FOREIGN KEY(teamid)
+      REFERENCES teams(team_id)
+      ON DELETE SET NULL
 );
 
 CREATE TABLE "userlookup" (
-  "userid" CHAR(37) NOT NULL,
-  "username" VARCHAR(255) NOT NULL
+  "userid" CHAR(37),
+  "username" VARCHAR(255) NOT NULL,
+  CONSTRAINT fk_userid
+    FOREIGN KEY(userid)
+      REFERENCES users(userid)
+      ON DELETE CASCADE
 );
 
 CREATE TABLE "teamlookup" (
-  "teamid" CHAR(37) NOT NULL,
-  "teamname" VARCHAR(255) NOT NULL
+  "teamid" CHAR(37),
+  "teamname" VARCHAR(255) NOT NULL,
+  CONSTRAINT fk_teamid
+    FOREIGN KEY teamid
+      REFERENCES teams(teamid)
+      ON DELETE CASCADE
 );
 
 CREATE TABLE "riot_info" (
-  "userid" CHAR(37) NOT NULL,
+  "userid" CHAR(37),
   "puuid" VARCHAR(78),
   "accountid" VARCHAR(56),
   "profileicon" INTEGER,
   "summonerlevel" SMALLINT,
-  "revisionDate" INTEGER
+  "revisionDate" INTEGER,
+  CONSTRAINT fk_userid
+    FOREIGN KEY userid
+      REFERENCES users(userid)
+      ON DELETE CASCADE
 );
-
-ALTER TABLE "userlookup" ADD FOREIGN KEY ("userid") REFERENCES "users" ("userid");
-
-ALTER TABLE "teamlookup" ADD FOREIGN KEY ("teamid") REFERENCES "teams" ("teamid");
-
-ALTER TABLE "riot_info" ADD FOREIGN KEY ("userid") REFERENCES "users" ("userid");
-
-ALTER TABLE "users" ADD FOREIGN KEY ("teamid") REFERENCES "teams" ("teamid");
